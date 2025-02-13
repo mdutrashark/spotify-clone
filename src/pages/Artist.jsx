d6db31ba903308c69856e701d4fd9b8f07aa149c
mdutrashark <marcos.md4199@gmail.com>
2025-02-13 12:15:44 -0300

    :construction: feat: Criada a funcionalidade dinâmica de atualização do banner e nome do artista da página Artista

--- src/pages/Artist.jsx
+++ src/pages/Artist.jsx
@@ -1,20 +1,31 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCirclePlay } from "@fortawesome/free-solid-svg-icons";
-import { Link } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import SongList from "../components/SongList";
+import { artistArray } from "../assets/database/artists";
+import { songsArray } from "../assets/database/songs";
 
 const Artist = () => {
+  const { id } = useParams();
+
+  const artistObj = artistArray.filter(
+    (currentArtistObj) => currentArtistObj.id === Number(id)
+  )[0];
+
+  const songsArrayFromArtist = songsArray.filter(
+    (currentSongsObj) => currentSongsObj.artist === artistObj.name
+  );
+
   return (
     <div className="artist">
       <div
         className="artist__header"
         style={{
-          backgroundImage:
-            "linear-gradient(to bottom, var(--_shade), var(--_shade)),url(https://i.scdn.co/image/ab67618600001016e2952bec9cfd4e6862e23607)",
+          backgroundImage: `linear-gradient(to bottom, var(--_shade), var(--_shade)),url(${artistObj.banner})`,
         }}
       >
-        <h2 className="artist__title">Jorge & Mateus</h2>
+        <h2 className="artist__title">{artistObj.name}</h2>
       </div>
 
       <div className="artist__body">
