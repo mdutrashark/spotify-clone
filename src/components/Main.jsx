ae691e921e878e22c237a99ce9d35c6b46d98df9
mdutrashark <marcos.md4199@gmail.com>
2025-02-12 22:53:37 -0300

    :sparkles: feat: Finalizadas as páginas Home, Artists e Songs.

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -3,26 +3,34 @@ import ItemList from "./ItemList";
 import { artistArray } from "../assets/database/artists";
 import { songsArray } from "../assets/database/songs";
 
-function Main() {
+const Main = ({ type }) => {
   return (
     <div className="main">
-      <ItemList
-        title="Artistas"
-        items={12}
-        itemsArray={artistArray}
-        path="/artists"
-        idPath="/artist"
-      />
+      {type === "artists" || type === undefined ? (
+        <ItemList
+          title="Artistas"
+          items={12}
+          itemsArray={artistArray}
+          path="/artists"
+          idPath="/artist"
+        />
+      ) : (
+        <></>
+      )}
 
-      <ItemList
-        title="Músicas"
-        items={24}
-        itemsArray={songsArray}
-        path="songs"
-        idPath="/song"
-      />
+      {type === "songs" || type === undefined ? (
+        <ItemList
+          title="Músicas"
+          items={24}
+          itemsArray={songsArray}
+          path="songs"
+          idPath="/song"
+        />
+      ) : (
+        <></>
+      )}
     </div>
   );
-}
+};
 
 export default Main;
